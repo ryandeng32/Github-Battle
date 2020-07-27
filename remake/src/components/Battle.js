d50a43399f6f5206a775505d316e3e45100daad4
Ryan Deng <ryan.deng32@gmail.com>
2020-07-27 12:30:03 -0400

    add PlayerPreview component

--- remake/src/components/Battle.js
+++ remake/src/components/Battle.js
@@ -64,6 +64,29 @@ const PlayerInput = () => {
     </form>
   );
 };
+
+const PlayerPreview = ({ username, onReset, label }) => {
+  return (
+    <div className="column player">
+      <h3 className="player-label">{label}</h3>
+      <div className="row bg-light">
+        <div className="player-info">
+          <img
+            className="avatar-small"
+            src={`https://github.com/${username}.png?size=200`}
+            alt={`Avatar for ${username}`}
+          />
+          <a href={`https://github.com/${username}`} className="link">
+            {username}
+          </a>
+        </div>
+        <button className="btn-clear flex-center" onClick={onReset}>
+          <FaTimesCircle color="rgb(194, 57, 42)" size={26} />
+        </button>
+      </div>
+    </div>
+  );
+};
 const Battle = () => {
   const [playerOne, setPlayerOne] = useState(null);
   const [playerTwo, setPlayerTwo] = useState(null);
